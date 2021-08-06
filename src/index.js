a4492c8d6075f68db3cb2f7db7d7f6774b486672
Tim Cross <theophilusx@gmail.com>
2021-08-06 13:55:05 +1000

    Add status and last-modified properties

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ const data = fs.readFileSync(icsFile, "utf-8");
 const expander = new IcalExpander({ ics: data, maxIterations: 1000 });
 
 const events = expander.between(
-  new Date("2020-01-01T00:00:00"),
+  new Date("2017-01-01T00:00:00"),
   new Date("2022-01-01T00:00:00")
 );
 
@@ -30,18 +30,33 @@ function parseAttendee(data) {
 }
 
 function parseDuration(d) {
+  function pad(v) {
+    return v < 10 ? `0${v}` : `${v}`;
+  }
+
   if (d.weeks) {
-    return `${d.weeks} week(s), ${d.days} day(s) and ${d.hours} hour(s) ${d.minutes} minutes`;
+    return `${d.weeks} wk ${d.days} d ${pad(d.hours)}:${pad(d.hours)} hh:mm`;
   } else if (d.days) {
-    return `${d.days} day(s) and ${d.hours} hour(s) ${d.minutes} minutes`;
+    return `${d.days} d ${pad(d.hours)}:${pad(d.minutes)} hh:mm`;
   } else {
-    return `${d.hours < 10 ? `0${d.hours}` : `${d.hours}`}:${
-      d.minutes < 10 ? `0${d.minutes}` : `${d.minutes}`
-    }`;
+    return `${pad(d.hours)}:${pad(d.minutes)} hh:mm`;
   }
 }
 
-function makeTimestamp(start, end) {
+function makeTimestamp(dt, type = "active") {
+  let start = "<";
+  let end = ">";
+
+  if (type === "inactive") {
+    start = "[";
+    end = "]";
+  }
+  const fmt = `${start}yyyy-LL-dd ccc HH:mm${end}`;
+  let date = DateTime.fromJSDate(dt);
+  return date.toFormat(fmt, { locale: "au" });
+}
+
+function makeTimestampRange(start, end) {
   const fmt = "<yyyy-LL-dd ccc HH:mm>";
   const sDate = DateTime.fromJSDate(start);
   const eDate = DateTime.fromJSDate(end);
@@ -50,21 +65,60 @@ function makeTimestamp(start, end) {
   })}`;
 }
 
+function makeMailtoLink(email) {
+  if (email && email.startsWith("mailto:")) {
+    let addr = email.substring(7);
+    return `[[${email}][${addr}]]`;
+  } else if (email && email.includes("@")) {
+    return `[[mailto:${email}][${email}]]`;
+  }
+  return email;
+}
+
+function dumpEvent(e) {
+  let data = [];
+  data.push(`* ${e.summary}`);
+  data.push(":PROPERTIES:");
+  data.push(":ICAL_EVENT:    t");
+  data.push(`:ID:            ${e.uid}`);
+  data.push(`:ORGANIZER:     ${makeMailtoLink(e.organizer)}`);
+  data.push(`:STATUS:        ${e.status}`);
+  data.push(`:LAST_MODIFIED: ${makeTimestamp(e.modified, "inactive")}`);
+  data.push(`:LOCATION:      ${e.location}`);
+  data.push(`:DURATION:      ${parseDuration(e.duration)}`);
+  data.push(
+    `:ATTENDEES:  ${e.attendees
+      .map((a) => `${makeMailtoLink(a.cn)} (${a.status})`)
+      .join(", ")}`
+  );
+  data.push(":END:");
+  data.push(makeTimestampRange(e.startDate, e.endDate));
+  data.push(`\n${e.description}`);
+  console.log(data.join("\n"));
+}
+
+function getPropertyValue(name, component) {
+  let prop = component.getAllProperties(name)[0];
+  if (prop) {
+    return prop.getValues()[0];
+  }
+  return "";
+}
+
 const mappedEvents = events.events.map((e) => ({
   attendees: e.attendees.map((a) => parseAttendee(a.jCal[1])),
   description: e.description,
   duration: e.duration,
   endDate: e.endDate.toJSDate(),
-  //exceptions: e.exceptions,
   location: e.location,
   organizer: e.organizer,
   recurrenceId: e.recurrenceId,
   sequence: e.sequence,
   startDate: e.startDate.toJSDate(),
-  //strictExceptions: e.strictExceptions,
   summary: e.summary,
   uid: e.uid,
-  status: e.status,
+  status: getPropertyValue("status", e.component),
+  modified: new Date(getPropertyValue("last-modified", e.component)),
 }));
 
 const mappedOccurrences = events.occurrences.map((o) => ({
@@ -72,30 +126,15 @@ const mappedOccurrences = events.occurrences.map((o) => ({
   startDate: o.startDate.toJSDate(),
   endDate: o.endDate.toJSDate(),
   summary: o.item.summary,
+  description: o.item.description,
   duration: o.item.duration,
   attendees: o.item.attendees.map((a) => parseAttendee(a.jCal[1])),
   location: o.item.location,
   organizer: o.item.organizer,
   uid: o.item.uid,
-  item: Object.keys(o.item),
+  status: getPropertyValue("status", o.item.component),
+  modified: getPropertyValue("last-modified", o.item.component),
 }));
 
-mappedEvents.map((e) => {
-  console.log(`* ${e.summary}`);
-  console.log(":PROPERTIES:");
-  console.log(":ICAL_EVENT: t");
-  console.log(`:ID:         ${e.uid}`);
-  console.log(`:ORGANIZER:  ${e.organizer}`);
-  console.log(`:LOCATION:   ${e.location}`);
-  console.log(`:DURATION:   ${parseDuration(e.duration)}`);
-  console.log(
-    `:ATTENDEES:  ${e.attendees.map((a) => `${a.cn} (${a.status})`).join(", ")}`
-  );
-  console.log(":END:");
-  console.log(makeTimestamp(e.startDate, e.endDate));
-  console.log(`\nStatus: ${e.status}`);
-  console.log(`\nDescription: ${e.description}`);
-  console.log("=================================");
-});
-
-mappedOccurrences.map((o) => console.dir(o));
+mappedEvents.forEach((e) => dumpEvent(e));
+mappedOccurrences.forEach((e) => dumpEvent(e));
